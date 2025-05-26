f0b87951740713ec693cc19379e2ff1eaa2bfcfa
Francisco Rocha <fa676533@gmail.com>
2025-05-26 13:32:30 -0600

    proyect with vite created

---
+++ baseApparel/src/main.js
@@ -0,0 +1,11 @@
+import './styles/normalize.css'
+import './styles/style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Base Apparel</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
